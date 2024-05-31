b2f052d14fc3e7f9796f2eeb791133a2f7c0f89e
xcigta <giampaolo.tranchida@vd.ch>
2024-05-31 15:54:59 +0200

    create Dockerfile

--- startmysql.sh
+++
@@ -1,9 +0,0 @@
-podman run -d \
---name mysql-8 \
--p 3306:3306 \
--v ~/mysql_data:/var/lib/mysql \
--e MYSQL_ROOT_PASSWORD='MyStr0ngP@ssw0rd' \
--e MYSQL_USER=go1 \
--e MYSQL_PASSWORD='go1' \
--e MYSQL_DATABASE=sample \
-mysql:8
